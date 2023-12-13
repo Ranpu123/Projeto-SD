4302aea2c72fcc12cb4c0481924d95ff3691708f
Vinícius Ranpu <vin8iciuscr27@gmail.com>
2023-12-13 12:20:14 -0300

    Fixes

---
+++ src/main/java/Server/UserConnected.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Server;
+
+/**
+ *
+ * @author vinic
+ */
+import java.util.Objects;
+
+public class UserConnected {
+    private String ip;
+    private String email;
+
+    public UserConnected(String ip, String email) {
+        this.ip = ip;
+        this.email = email;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserConnected that = (UserConnected) o;
+        return Objects.equals(ip, that.ip);
+    }
+    
+    
+    
+}
